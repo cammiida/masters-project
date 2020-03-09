1f6492feae94165927c46d66518d540b562a3051
Camilla <cammiida@gmail.com>
2020-03-09 12:12:48 +0100

    :sparkles:Added rest of code for decoder, encoder and running training/validation

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+'''
+Parts of this code were incorporated from the following github repositories:
+1. parksunwoo/show_attend_and_tell_pytorch
+Link: https://github.com/parksunwoo/show_attend_and_tell_pytorch/blob/master/prepro.py
+2. sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning
+Link: https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning
+3. ajamjooom/Image-Captions/data_loader.py
+Link: https://github.com/ajamjoom/Image-Captions/blob/master/data_loader.py
+
+This script has the Encoder and Decoder models and training/validation scripts.
+Edit the parameters sections of this file to specify which models to load/run
+'''
+
 import pickle
 import torch.nn as nn
 import torch
-from transformers import AlbertTokenizer, AlbertModel, AlbertForMaskedLM, BertTokenizer, BertModel, BertForMaskedLM
-import imageio
-from torchtext.vocab import Vectors, GloVe
+from torch.nn.utils.rnn import pack_padded_sequence
+from data_loader import get_loader
+from nltk.translate.bleu_score import corpus_bleu
+from tqdm import tqdm
 import torchvision.models as models
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.transform
+from PIL import Image
+from transformers import AlbertTokenizer, AlbertModel
+import imageio
 
 
 ###################
@@ -22,14 +42,26 @@ decoder_lr = 0.0004
 glove_model = False
 bert_model = False
 
-from_checkpoint = True
-train_model = False
-valid_model = True
-eval = True
+from_checkpoint = False
+train_model = True
+valid_model = False
 
 ###################
 # END Parameters
 ###################
+
+# loss
+class loss_obj(object):
+    def __init__(self):
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
+
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
 
 # OPTIONAL: if you want to have more information on what's happening under the hood, activate the logger as follows
 import logging
@@ -40,10 +72,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load pretrained model tokenizer (vocabulary)
 tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
-#tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # Load pre-trained model (weights)
-model = AlbertModel.from_pretrained('albert-base-v2')
+model = AlbertModel.from_pretrained('albert-base-v2').to(device)
+model.eval()
 
 # Load GloVe
 glove_vectors = pickle.load(open('glove.6B/glove_words.pkl', 'rb'))
@@ -144,9 +176,10 @@ class Decoder(nn.Module):
                 tokens_tensor = torch.tensor([indexed_tokens]).to(device)
 
                 with torch.no_grad():
-                    encoded_layers, _ = AlbertModel(tokens_tensor)
+                    encoded_layers, _ = model(tokens_tensor)
 
-                # Remove all dims with dim_size = 0 in dim 11
+                # TODO: Figure out why 11
+                # Squeeze away first dimension in dim 12 (idx 11) if dim size is 1
                 albert_embedding = encoded_layers[11].squeeze(0)
 
                 split_cap = cap.split()
@@ -193,8 +226,32 @@ class Decoder(nn.Module):
         predictions = torch.zeros(batch_size, max_dec_len, vocab_size).to(device)
         alphas = torch.zeros(batch_size, max_dec_len, num_pixels).to(device)
 
+        for t in range(max(dec_len)):
+            batch_size_t = sum([l > t for l in dec_len])
 
-# vocab indices
+            # soft-attention
+            enc_att = self.enc_att(encoder_out[:batch_size_t])
+            dec_att = self.dec_att(h[:batch_size_t])
+            att = self.att(self.relu(enc_att + dec_att.unsqueeze(1))).squeeze(2)
+            alpha = self.softmax(att)
+            attention_weighted_encoding = (encoder_out[:batch_size_t] * alpha.unsqueeze(2)).sum(dim=1)
+
+            gate = self.sigmoid(self.f_beta(h[:batch_size_t]))
+            attention_weighted_encoding = gate * attention_weighted_encoding
+
+            batch_embeds = embeddings[:batch_size_t, t, :]
+            cat_val = torch.cat([batch_embeds.double(), attention_weighted_encoding.double()], dim=1)
+
+            h, c = self.decode_step(cat_val.float(), (h[:batch_size_t].float(), c[:batch_size_t].float()))
+            preds = self.fc(self.dropout(h))
+            predictions[:batch_size_t, t, :] = preds
+            alphas[:batch_size_t, t, :] = alpha
+
+        # preds, sorted capts, dec lens, attention weights
+        return predictions, encoded_captions, dec_len, alphas
+
+
+    # vocab indices
 PAD = 0
 START = 1
 END = 2
@@ -203,3 +260,269 @@ UNK = 3
 # Load vocabulary
 with open('data/vocab.pkl', 'rb') as f:
     vocab = pickle.load(f)
+
+# load data
+train_loader = get_loader('train', vocab, batch_size)
+val_loader = get_loader('val', vocab, batch_size)
+
+
+#############
+# Init model
+#############
+
+criterion = nn.CrossEntropyLoss().to(device)
+
+if from_checkpoint:
+    encoder = Encoder().to(device)
+    decoder = Decoder(vocab_size=len(vocab), use_glove=glove_model, use_bert=bert_model).to(device)
+
+    if torch.cuda.is_available():
+        if bert_model:
+            print('Pre-Trained BERT Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_bert')
+            decoder_checkpoint = torch.load('./checkpoints/decoder_bert')
+        elif glove_model:
+            print('Pre-Trained GloVe Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_glove')
+            decoder_checkpoint = torch.load('./checkpoints/decover_glove')
+        else:
+            print('Pre-Trained Baseline Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_baseline')
+            decoder_checkpoint = torch.load('./checkpoints/decoder_baseline')
+    else:
+        if bert_model:
+            print('Pre-Trained BERT Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_bert', map_location='cpu')
+            decoder_checkpoint = torch.load('./checkpoints/decoder_bert', map_location='cpu')
+        elif glove_model:
+            print('Pre-Trained GloVe Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_glove', map_location='cpu')
+            decoder_checkpoint = torch.load('./checkpoints/decoder_glove', map_location='cpu')
+        else:
+            print('Pre-Trained Baseline Model')
+            encoder_checkpoint = torch.load('./checkpoints/encoder_baseline', map_location='cpu')
+            decoder_checkpoint = torch.load('./checkpoints/decoder_baseline', map_location='cpu')
+
+    encoder.load_state_dict(encoder_checkpoint['model_state_dict'])
+    decoder_optimizer = torch.optim.Adam(params=decoder.parameters(), lr=decoder_lr)
+    decoder.load_state_dict(decoder_checkpoint['model_state_dict'])
+    decoder_optimizer.load_state_dict(decoder_checkpoint['optimizer_state_dict'])
+
+else:
+    encoder = Encoder().to(device)
+    decoder = Decoder(vocab_size=len(vocab), use_glove=glove_model, use_bert=bert_model).to(device)
+    decoder_optimizer = torch.optim.Adam(params=decoder.parameters(), lr=decoder_lr)
+
+
+###############
+# Train model
+###############
+
+def train():
+    print("Started training...")
+    for epoch in tqdm(range(num_epochs)):
+        decoder.train()
+        encoder.train()
+
+        losses = loss_obj()
+        num_batches = len(train_loader)
+
+        for i, (imgs, caps, caplens) in enumerate(tqdm(train_loader)):
+
+            imgs = encoder(imgs.to(device))
+            caps = caps.to(device)
+
+            scores, caps_sorted, decode_lengths, alphas = decoder(imgs, caps, caplens)
+            scores = pack_padded_sequence(scores, decode_lengths, batch_first=True)[0]
+
+            targets = caps_sorted[:, 1:]
+            targets = pack_padded_sequence(targets, decode_lengths, batch_first=True)[0]
+
+            loss = criterion(scores, targets).to(device)
+
+            loss += ((1. - alphas.sum(dim=1)) ** 2).mean()
+
+            decoder_optimizer.zero_grad()
+            loss.backward()
+
+            # grad_clip decoder
+            for group in decoder_optimizer.param_groups:
+                for param in group['params']:
+                    if param.grad is not None:
+                        param.grad.data.clamp_(-grad_clip, grad_clip)
+
+            decoder_optimizer.step()
+
+            losses.update(loss.item(), sum(decode_lengths))
+
+            # save model each 100 batches
+            if i % 5000 == 0 and i != 0:
+                print('epoch ' + str(epoch + 1) + '/4 ,Batch ' + str(i) + '/' + str(num_batches) + ' loss:' + str(
+                    losses.avg))
+
+                # adjust learning rate (create condition for this)
+                for param_group in decoder_optimizer.param_groups:
+                    param_group['lr'] = param_group['lr'] * 0.8
+
+                print('saving model...')
+
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': decoder.state_dict(),
+                    'optimizer_state_dict': decoder_optimizer.state_dict(),
+                    'loss': loss,
+                }, './checkpoints/decoder_mid')
+
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': encoder.state_dict(),
+                    'loss': loss,
+                }, './checkpoints/encode_mid')
+
+                print('model saved')
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': decoder.state_dict(),
+            'optimizer_state_dict': decoder_optimizer.state_dict(),
+            'loss': loss,
+        }, './checkpoints/decoder_epoch' + str(epoch + 1))
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': encoder.state_dict(),
+            'loss': loss,
+        }, './checkpoints/encoder_epoch' + str(epoch + 1))
+
+        print('epoch checkpoint saved')
+
+    print("Completed training...")
+
+
+#################
+# Validate model
+#################
+
+def print_sample(hypotheses, references, test_references, imgs, alphas, k, show_att, losses):
+    bleu_1 = corpus_bleu(references, hypotheses, weights=(1, 0, 0, 0))
+    bleu_2 = corpus_bleu(references, hypotheses, weights=(0, 1, 0, 0))
+    bleu_3 = corpus_bleu(references, hypotheses, weights=(0, 0, 1, 0))
+    bleu_4 = corpus_bleu(references, hypotheses, weights=(0, 0, 0, 1))
+
+    print("Validation loss: " + str(losses.avg))
+    print("BLEU-1: " + str(bleu_1))
+    print("BLEU-2: " + str(bleu_2))
+    print("BLEU-3: " + str(bleu_3))
+    print("BLEU-4: " + str(bleu_4))
+
+    img_dim = 336  # 14*24
+
+    hyp_sentence = []
+    for word_idx in hypotheses[k]:
+        hyp_sentence.append(vocab.idx2word[word_idx])
+
+    ref_sentence = []
+    for word_idx in test_references[k]:
+        ref_sentence.append(vocab.idx2word[word_idx])
+
+    print('Hypotheses: ' + " ".join(hyp_sentence))
+    print('References: ' + " ".join(ref_sentence))
+
+    img = imgs[0][k]
+    imageio.imwrite('img.jpg', img)
+
+    if show_att:
+        image = Image.open('img.jpg')
+        image = image.resize([img_dim, img_dim], Image.LANCZOS)
+        for t in range(len(hyp_sentence)):
+
+            plt.subplot(np.ceil(len(hyp_sentence) / 5.), 5, t + 1)
+
+            plt.text(0, 1, '%s' % (hyp_sentence[t]), color='black', backgroundcolor='white', fontsize=12)
+            plt.imshow(image)
+            current_alpha = alphas[0][t, :].detach().numpy()
+            alpha = skimage.transform.resize(current_alpha, [img_dim, img_dim])
+            if t == 0:
+                plt.imshow(alpha, alpha=0)
+            else:
+                plt.imshow(alpha, alpha=0.7)
+            plt.axis('off')
+    else:
+        img = imageio.imread('img.jpg')
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+
+
+def validate():
+    references = []
+    test_references = []
+    hypotheses = []
+    all_imgs = []
+    all_alphas = []
+
+    print("Started validation...")
+    decoder.eval()
+    encoder.eval()
+
+    losses = loss_obj()
+
+    num_batches = len(val_loader)
+    # Batches
+    for i, (imgs, caps, caplens) in enumerate(tqdm(val_loader)):
+
+        imgs_jpg = imgs.numpy()
+        imgs_jpg = np.swapaxes(np.swapaxes(imgs_jpg, 1, 3), 1, 2)
+
+        # Forward prop.
+        imgs = encoder(imgs.to(device))
+        caps = caps.to(device)
+
+        scores, caps_sorted, decode_lengths, alphas = decoder(imgs, caps, caplens)
+        targets = caps_sorted[:, 1:]
+
+        # Remove timesteps that we didn't decode at, or are pads
+        scores_packed = pack_padded_sequence(scores, decode_lengths, batch_first=True)[0]
+        targets_packed = pack_padded_sequence(targets, decode_lengths, batch_first=True)[0]
+
+        # Calculate loss
+        loss = criterion(scores_packed, targets_packed)
+        loss += ((1. - alphas.sum(dim=1)) ** 2).mean()
+        losses.update(loss.item(), sum(decode_lengths))
+
+        # References
+        for j in range(targets.shape[0]):
+            img_caps = targets[j].tolist()  # validation dataset only has 1 unique caption per img
+            clean_cap = [w for w in img_caps if w not in [PAD, START, END]]  # remove pad, start, and end
+            img_captions = list(map(lambda c: clean_cap, img_caps))
+            test_references.append(clean_cap)
+            references.append(img_captions)
+
+        # Hypotheses
+        _, preds = torch.max(scores, dim=2)
+        preds = preds.tolist()
+        temp_preds = list()
+        for j, p in enumerate(preds):
+            pred = p[:decode_lengths[j]]
+            pred = [w for w in pred if w not in [PAD, START, END]]
+            temp_preds.append(pred)  # remove pads, start, and end
+        preds = temp_preds
+        hypotheses.extend(preds)
+
+        if i == 0:
+            all_alphas.append(alphas)
+            all_imgs.append(imgs_jpg)
+
+    print("Completed validation...")
+    print_sample(hypotheses, references, test_references, all_imgs, all_alphas, 1, False, losses)
+
+
+######################
+# Run training/validation
+######################
+
+if train_model:
+    train()
+
+if valid_model:
+    validate()
--- test-albert.py
+++ test-albert.py
@@ -23,43 +23,72 @@ def test_albert_tokenization(text):
 
     return tokens_tensor, segments_tensors
 
-def test_bert_tokenization(text):
-    tokenized_text = bert_tokenizer.tokenize(text)
-    print('BERT: ', tokenized_text)
+def test_bert_tokenization(cap):
+    tokenized_cap = bert_tokenizer.tokenize(cap)
+    print('cap: ', cap)
+    print('tokenized_cap: ', tokenized_cap)
+
 
     # Convert token to vocabulary indices
-    indexed_tokens = albert_tokenizer.convert_tokens_to_ids(tokenized_text)
+    indexed_tokens = albert_tokenizer.convert_tokens_to_ids(tokenized_cap)
     # Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
-    segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
 
     # Convert inputs to PyTorch tensors
     tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
 
     with torch.no_grad():
-        encoded_captions, _ = model(tokens_tensor, segments_tensors)
+        encoded_layers, _ = model(tokens_tensor)
 
-    # Load vocabulary
-    with open('./data/vocab.pkl', 'rb') as f:
-        vocab = pickle.load(f)
+    print('encoded_layers: ', encoded_layers)
+    print(encoded_layers.size())
+    print(encoded_layers.squeeze(0).size())
+    bert_embedding = encoded_layers.squeeze(0)
 
-    for cap_idx in encoded_captions:
-        print(cap_idx)
-        while len(cap_idx) < 12:
-            cap_idx.append(0)
+    print('bert_embedding: ', bert_embedding)
 
-        cap = ' '.join([vocab.idx2word[word_idx.item()] for word_idx in cap_idx])
-        cap = u'[CLS] ' + cap
+    embeddings = []
 
-        tokenized_cap = bert_tokenizer.tokenize(cap)
-        indexed_tokens = bert_tokenizer.convert_tokens_to_ids(tokenized_cap)
-        tokens_tensor = torch.tensor([indexed_tokens])
+    split_cap = cap.split()
+    tokens_embedding = []
+    j = 0
 
-        with torch.no_grad():
-            encoded_layers, _ = model(tokens_tensor)
+    for full_token in split_cap:
+        curr_token = ''
+        x = 0
+        print('full_token: ', full_token)
+        for i,_ in enumerate(tokenized_cap[1:]): # disregard CLS
+            #print('i, j: ', i, j)
+            token = tokenized_cap[i+j]
+            piece_embedding = bert_embedding[i+j]
+            # print('token: ', token)
+            # print('piece_embedding: ', piece_embedding)
+
+            # full token
+            if token == full_token and curr_token == '':
+                tokens_embedding.append(piece_embedding)
+                j += 1
+            else: # partial token
+                x += 1
+                if curr_token == '':
+                    tokens_embedding.append(piece_embedding)
+                    curr_token += token.replace('#', '')
+                else:
+                    tokens_embedding[-1] = torch.add(tokens_embedding[-1], piece_embedding)
+                    curr_token += token.replace('#', '')
+
+                    if curr_token == full_token: # end of partial
+                        j += x
+                        break
+
+    cap_embeddings = torch.stack(tokens_embedding)
+    print('cap_embeddings size: ', cap_embeddings.size())
+    embeddings.append(cap_embeddings)
+
+    embeddings = torch.stack(embeddings)
+
+    print('embeddings: ', embeddings)
 
 
-    return encoded_layers
 
 def test_albert_embedding_squeeze(tokens_tensor, segments_tensors):
     with torch.no_grad():
@@ -72,12 +101,27 @@ def test_bert_embedding(tokens_tensor, segments_tensors):
         encoded_layers, _ = model(tokens_tensor, segments_tensors)
 
 
+def test_encode_plus(text_a, text_b):
+    padded_sequence_a = albert_tokenizer.encode_plus(text_a, max_length=19, pad_to_max_length=True)
+    padded_sequence_b = albert_tokenizer.encode_plus(text_b, max_length=19, pad_to_max_length=True)
+    padded_sequence = albert_tokenizer.encode_plus(text_a, text_b, max_length=19, pad_to_max_length=True)
+    print('padded_sequence_a: ', padded_sequence_a)
+    print('padded_sequence_b: ', padded_sequence_b)
+    print('padded_sequences: ', padded_sequence)
+
+def text_encoded_sequence(text_a, text_b):
+    encoded_sequence = albert_tokenizer.encode(text_a, text_b)
+    print(albert_tokenizer.decode(encoded_sequence ))
+
+    return albert_tokenizer.decode(encoded_sequence)
+
 if __name__ == '__main__':
-    encoded_layers = test_albert_tokenization("[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]")
-    #encoded_layers = test_albert_embedding_squeeze(tokens_tensor, segments_tensors)
-    print(encoded_layers)
-    print(encoded_layers[1])
-    print(encoded_layers[1].squeeze(0))
+    text_a = "Who was Jim Henson ?"
+    text_b = "Jim Henson was a puppeteer"
+    # test_bert_tokenization("[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]")
+    # encoded_layers = test_albert_embedding_squeeze(tokens_tensor, segments_tensors)
+    test_encode_plus(text_a, text_b)
+    # text_encoded_sequence(text_a, text_b)
 
 
 
