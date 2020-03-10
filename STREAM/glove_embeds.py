0a317a48fa6ddf484f61a57451f540f7f6c59a07
Camilla <cammiida@gmail.com>
2020-03-10 11:32:46 +0100

    :bug::art:Updated requirements and added Vocabulary import to glove_embeds.py

--- STREAM/glove_embeds.py
+++ STREAM/glove_embeds.py
@@ -9,51 +9,75 @@ import pickle
 import numpy as np
 import bcolz
 import os
+from processData import Vocabulary
 
-words = []
-idx = 0
-word2idx = {}
-root_dir = '../data/glove.6B/'
-vectors = bcolz.carray(np.zeros(1), rootdir=os.path.join(root_dir, '6B.300.dat'), mode='w')
 
-with open(os.path.join(root_dir, 'glove.6B.300d.txt'), 'rb') as f:
-    for l in f:
-        line = l.decode().split()
-        word = line[0]
-        words.append(word)
-        word2idx[word] = idx
-        idx += 1
-        vect = np.array(line[1:]).astype(np.float)
-        vectors.append(vect)
+def create_glove_files(root_dir):
 
-vectors = bcolz.carray(vectors[1:].reshape((400000, 300)), rootdir=os.path.join(root_dir, '6B.300.dat'), mode='w')
-vectors.flush()
-pickle.dump(words, open(os.path.join(root_dir, '6B.300_words.pkl'), 'wb'))
-pickle.dump(word2idx, open(os.path.join(root_dir, '6B.300_idx.pkl'), 'wb'))
+    words = []
+    idx = 0
+    word2idx = {}
 
-with open('../data/vocab.pkl', 'rb') as f:
-    vocab = pickle.load(f)
+    vectors = bcolz.carray(np.zeros(1), rootdir=os.path.join(root_dir, '6B.300.dat'), mode='w')
 
-print('Loading vocab...')
+    with open(os.path.join(root_dir, 'glove.6B.300d.txt'), 'rb') as f:
+        for l in f:
+            line = l.decode().split()
+            word = line[0]
+            words.append(word)
+            word2idx[word] = idx
+            idx += 1
+            vect = np.array(line[1:]).astype(np.float)
+            vectors.append(vect)
 
-vectors = bcolz.open(os.path.join(root_dir, '6B.300.dat'))[:]
-words = pickle.load(open(os.path.join(root_dir, '6B.300_words.pkl'), 'rb'))
-word2idx = pickle.load(open(os.path.join(root_dir, '6B.300_idx.pkl'), 'rb'))
+    vectors = bcolz.carray(vectors[1:].reshape((400000, 300)), rootdir=os.path.join(root_dir, '6B.300.dat'), mode='w')
+    vectors.flush()
+    pickle.dump(words, open(os.path.join(root_dir, '6B.300_words.pkl'), 'wb'))
+    pickle.dump(word2idx, open(os.path.join(root_dir, '6B.300_idx.pkl'), 'wb'))
 
-print('glove is loaded...')
 
-glove = {w: vectors[word2idx[w]] for w in words}
-matrix_len = len(vocab)
-weights_matrix = np.zeros((matrix_len, 300))
-words_found = 0
+def load_glove_files(root_dir):
+    print('Loading GloVe files...')
+    vectors = bcolz.open(os.path.join(root_dir, '6B.300.dat'))[:]
+    words = pickle.load(open(os.path.join(root_dir, '6B.300_words.pkl'), 'rb'))
+    word2idx = pickle.load(open(os.path.join(root_dir, '6B.300_idx.pkl'), 'rb'))
 
-for i, word in enumerate(vocab.idx2word):
-    try:
-        weights_matrix[i] = glove[word]
-        words_found += 1
-    except KeyError:
-        weights_matrix[i] = np.random.normal(scale=0.6, size=(300, ))
+    print('GloVe files are loaded')
 
-pickle.dump(weights_matrix, open(os.path.join(root_dir, 'glove_words.pkl'), 'wb'), protocol=2)
+    return vectors, words, word2idx
 
-print('weights_matrix is created')
+
+def generate_weight_matrix(root_dir, glove_dir, vectors, words, word2idx):
+
+    print('Loading vocab...')
+    with open(os.path.join(root_dir, 'vocab.pkl'), 'rb') as f:
+        vocab = pickle.load(f)
+    print('Vocab loaded')
+
+    glove = {w: vectors[word2idx[w]] for w in words}
+    matrix_len = len(vocab)
+    weights_matrix = np.zeros((matrix_len, 300))
+    words_found = 0
+
+    for i, word in enumerate(vocab.idx2word):
+        try:
+            weights_matrix[i] = glove[word]
+            words_found += 1
+        except KeyError:
+            weights_matrix[i] = np.random.normal(scale=0.6, size=(300, ))
+
+    print('Creating weights_matrix...')
+
+    pickle.dump(weights_matrix, open(os.path.join(glove_dir, 'glove_words.pkl'), 'wb'), protocol=2)
+
+    print('weights_matrix is created')
+
+
+if __name__ == '__main__':
+    # TODO: Get paths from config
+    data_dir = '../data/'
+    glove_dir = os.path.join(data_dir, 'glove.6B/')
+
+    create_glove_files(glove_dir)
+    vectors, words, word2idx = load_glove_files(glove_dir)
+    generate_weight_matrix(data_dir, glove_dir, vectors, words, word2idx)
