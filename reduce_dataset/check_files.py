48c930621816f372ca5e2a97df98e0bcb5b92cbd
Camilla <cammiida@gmail.com>
2020-03-16 18:43:06 +0100

    :bug:Fix bug where filenames were not loaded correctly and deletion from json files was incorrectly implemented

--- reduce_dataset/check_files.py
+++ reduce_dataset/check_files.py
@@ -2,15 +2,18 @@ import os
 import json
 import pickle
 from STREAM.processData import Vocabulary
-
+from random import randint
+from tqdm import tqdm, trange
+import copy
 
 def check_json(path):
     with open(path, 'rb') as json_file:
         data = json.load(json_file)
+        print("num images: ", len(data["images"]))
         for key, value in data.items():
             print(key)
             if isinstance(data[key], list):
-                print(data[key][0])
+                print(data[key][0:2])
 
 
 def check_pickle(path):
@@ -57,16 +60,69 @@ def json_matching_files(json_path, files_dir):
     print("only caption: \nLength: ", len(only_caption), "list: ", only_caption)
     print("only file: \nLength: ", len(only_file), "list: ", only_file)
 
+def check_caption_val2014():
+    caption_file_path = '../data/small1/annotations/captions_val2014.json'
+    with open(caption_file_path, 'rb') as f:
+        captions_dict = json.load(f, encoding='utf-8')
+
+    images = captions_dict["images"]
+    annotations = captions_dict["annotations"]
+
+    idx = randint(0, len(images) - 1)
+    img_id = images[idx]["id"]
+    filename = images[idx]["file_name"].replace('.jpg', '')
+    print("filename: ", filename)
+    print("img_id: ", img_id)
+
+    ann_dict = next(ann for ann in annotations if ann["image_id"] == img_id)
+    print(ann_dict)
+    cap = ann_dict["caption"]
+    print("Caption: ", cap)
+
+
+def check_create_json(old_root, new_root):
+    keys = {
+        "images": {"check_key": "file_name", "del_key": "id", "extension": ".jpg"},
+        "annotations": {"check_key": "image_id", "del_key": "category_id", "extension": ""}
+    }
+    dir_name = 'annotations'
+    json_name = 'captions_val2014.json'
+    old_json_path = os.path.join(old_root, dir_name, json_name)
+    new_json_path = os.path.join(new_root, dir_name, json_name)
+    tqdm.write("Creating new json file %s from %s..." % (new_json_path, old_json_path))
+
+    with open(old_json_path, 'rb') as old_json:
+        data = json.load(old_json)
+        new_json = copy.deepcopy(data)
+
+    lengths = dict()
+    del_list = []
+    for key, value in tqdm(keys.items()):
+        lengths[key] = len(new_json[key])
+        tqdm.write("key: %s" % key)
+        tqdm.write("value: %s" % value)
+        tqdm.write("value['check_key']: %s" % value["check_key"])
+        tqdm.write("randint: %d" % randint(0, len(new_json["images"])-1))
+
+
+
 
 if __name__ == '__main__':
     big_root = '../data/big'
     small_root = '../data/small'
 
-    check_json(os.path.join(big_root, 'annotations/instances_train2014.json'))
-    check_json(os.path.join(small_root, 'annotations/captions_train2014.json'))
+    #check_create_json(big_root, small_root)
+    #check_caption_val2014()
+    #check_pickle(os.path.join(small_root, 'train/filenames.pickle'))
+    #check_pickle(os.path.join(small_root, 'test/filenames.pickle'))
+
+    # check_json(os.path.join(big_root, 'annotations/instances_train2014.json'))
+    # check_json(os.path.join(big_root, 'annotations/instances_val2014.json'))
     # check_pickle(os.path.join(big_root, 'captions.pickle'))
     # check_text_file(os.path.join(big_root, 'example_captions.txt'))
     # check_text_file(os.path.join(big_root, 'example_filenames.txt'))
 
     #json_matching_files(os.path.join(small_root, 'annotations/captions_train2014.json'),
     #                    os.path.join(small_root, 'train2014'))
+    print(len(os.listdir('../data/big/text/')))
+    print(len(os.listdir('../data/small/text/')))
--- reduce_dataset/resize_dataset.py
+++ reduce_dataset/resize_dataset.py
@@ -11,37 +11,40 @@ from tqdm import tqdm, trange
 ######################
 #   HELPER METHODS   #
 ######################
-def delete_from_dict(obj_dict, obj_key, check_key, control_list, extension='', del_key=None):
+def filter_dict(obj_dict, obj_key, check_key, control_list, extension='', control_key=None):
     i = 0
     count = 1
-    del_list = []
+    new_ctrl_list = []
 
-    objects = obj_dict[obj_key]
-    org_len = len(objects)
+    objects = obj_dict[obj_key] # e.g. annotations
+    org_len = len(objects) # len(annotations)
     while i < len(objects):
 
-        obj = objects[i]
-        value = obj[check_key]
+        obj = objects[i] # e.g. annotation
+        value = obj[check_key] # for annotations: image_id
         if isinstance(value, str): value = value.replace(extension, '')
 
-        if value not in control_list:
-            if del_key is not None: del_list.append(obj[del_key])
-            objects.pop(i)
-        else:
+        if value in control_list: # list of image ids for annotations
+            # if there is a control key, add value of control_key to new control list and increment i
+            if control_key is not None: new_ctrl_list.append(obj[control_key])
             i += 1
-
+        # if file_name was not in filenames list, remove it from list and don't increment i
+        else:
+            objects.pop(i)
+        '''
         count += 1
         if count % 1000 == 0 or count == org_len:
-            print("Number of ", obj_key, " checked: ", count, "/", org_len )
+            tqdm.write("Number of %s checked: %d/%d" % (obj_key, count, org_len) )
+        '''
+    return obj_dict, new_ctrl_list
 
 
-    return obj_dict, del_list if len(del_list) > 0 else obj_dict
-
-
-def load_pickle(path):
+def load_pickle(directory, filename):
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    path = os.path.join(directory, filename)
     with open(path, 'rb') as f:
         filenames = pickle.load(f, encoding='utf-8')
-        print(filenames[:10])
     return filenames
 
 
@@ -50,18 +53,36 @@ def save_pickle(data, directory, filename):
         create_dir(directory)
 
     file_path = os.path.join(directory, filename)
+    if os.path.isfile(file_path):
+        tqdm.write("Pickle already exists! Deleting...")
+        os.remove(file_path)
+    tqdm.write("Saving new pickle...")
     with open(file_path, 'wb') as f:
         pickle.dump(data, f)
 
+    tqdm.write("New pickle saved to %s!" % file_path)
+
 def load_json(path):
     with open(path, 'rb') as f:
-        data = json.load(f, encoding='utf-8')
+        data = json.load(f)
 
     return data
 
 
+def get_train_test_filenames(root):
+    train_dir = os.path.join(root, 'train')
+    test_dir = os.path.join(root, 'test')
+    file_name = 'filenames.pickle'
+
+    train_filenames = load_pickle(train_dir, file_name)
+    test_filenames = load_pickle(test_dir, file_name)
+
+    return train_filenames, test_filenames
+
+
 # percentile < 1
 def generate_sample(filenames, percentile):
+    # Get filenames
     assert percentile < 1
     k = math.floor(len(filenames) * percentile)
     sample = random.sample(filenames, k)
@@ -71,41 +92,47 @@ def generate_sample(filenames, percentile):
 def check_json(file):
     with open(file, 'rb') as json_file:
         data = json.load(json_file)
-        print(data["categories"][:10])
+        tqdm.write(data["categories"][:10])
         for key, value in data.items():
-            print("Key: ", key)
+            tqdm.write("Key: ", key)
+
 
 def create_dir(path):
-    print("create dir: %s" % path)
+    tqdm.write("create dir: %s" % path)
     try:
         os.mkdir(path)
     except OSError:
-        print("Creation of the directory %s failed" % path)
+        tqdm.write("Creation of the directory %s failed" % path)
     else:
-        print("Successfully created the directory %s" % path)
+        tqdm.write("Successfully created the directory %s" % path)
+
 
 ##########################
 #   END HELPER METHODS   #
 ##########################
 
-def get_sample_filenames(data_dir, percentile):
-    print("Getting sample filenames...")
-    train_names = load_pickle(os.path.join(data_dir, 'train', 'filenames.pickle'))
-    test_names = load_pickle(os.path.join(data_dir, 'test', 'filenames.pickle'))
+def generate_sample_filenames(source, percentile):
+    tqdm.write("Getting sample filenames...")
+    train_dir = os.path.join(source, 'train')
+    test_dir = os.path.join(source, 'test')
+    file_name = 'filenames.pickle'
+    train_filenames = load_pickle(train_dir, file_name)
+    test_filenames = load_pickle(test_dir, file_name)
 
-    sample_train_names = generate_sample(train_names, percentile)
-    sample_test_names = generate_sample(test_names, percentile)
+    sample_train_filenames = generate_sample(train_filenames, percentile)
+    sample_test_filenames = generate_sample(test_filenames, percentile)
 
-    return sample_train_names, sample_test_names
+    return sample_train_filenames, sample_test_filenames
 
 
 def save_samples(destination, train_sample, test_sample):
+    tqdm.write("Saving sample filenames in filenames.pickle...")
     save_pickle(train_sample, os.path.join(destination, 'train'), 'filenames.pickle')
     save_pickle(test_sample, os.path.join(destination, 'test'), 'filenames.pickle')
 
 
 def copy_files(source, destination, filenames, extension):
-    print("Copying files from %s to %s..." % (source, destination))
+    tqdm.write("Copying files from %s to %s..." % (source, destination))
 
     files = os.listdir(source)
     for i, f in enumerate(tqdm(files)):
@@ -115,107 +142,136 @@ def copy_files(source, destination, filenames, extension):
 
             copyfile(src, dst)
 
-            #if os.path.isfile(dst):
-            #    print("Copied file %s to %s." % (src, dst))
             if not os.path.isfile(dst):
-                print("Could not copy file %s to %s." % (src, dst))
+                tqdm.write("Could not copy file %s to %s." % (src, dst))
 
 
-def create_json(old_json_path, new_json_path, filenames, keys):
-    print("Creating new json file %s from %s..." % (old_json_path, new_json_path))
+def create_json(old_root, new_root, json_name, filenames, keys):
+    dir_name = 'annotations'
+    old_json_path = os.path.join(old_root, dir_name, json_name)
+    new_json_path = os.path.join(new_root, dir_name, json_name)
+    tqdm.write("Creating new json file %s from %s..." % (new_json_path, old_json_path))
 
     with open(old_json_path, 'rb') as old_json:
         data = json.load(old_json)
         new_json = copy.deepcopy(data)
 
     lengths = dict()
+    control_list = []
     for key, value in tqdm(keys.items()):
         lengths[key] = len(new_json[key])
 
-        new_json, del_list = delete_from_dict(obj_dict=new_json, obj_key=key, check_key=value["check_key"],
-                                              control_list=filenames, extension=value["extension"], del_key=value["del_key"])
+        new_json, control_list = filter_dict(obj_dict=new_json,
+                                         obj_key=key,
+                                         check_key=value["check_key"],
+                                         control_list=filenames if key=="images" else control_list,
+                                         extension=value["extension"],
+                                         control_key=value["control_key"])
+    tqdm.write("new_json after all deletion: %s" % new_json)
 
     for key, value in keys.items():
-        print("Length of %s before start: %s" % (key, str(lengths[key])))
-        print("Length of %s after run: %s" % (key, str(len(new_json[key]))))
+        tqdm.write("Length of %s before start: %s" % (key, str(lengths[key])))
+        tqdm.write("Length of %s after run: %s" % (key, str(len(new_json[key]))))
 
+    new_dir_path = os.path.join(new_root, dir_name)
+    if not os.path.isdir(new_dir_path):
+        os.mkdir(new_dir_path)
+    if os.path.isfile(new_json_path):
+        os.remove(new_json_path)
     with open(new_json_path, 'w', encoding='utf-8') as outfile:
         json.dump(new_json, outfile)
 
-        print("Saved json to: ", new_json_path)
+        tqdm.write("Saved json to: %s " % new_json_path)
 
 
-def create_annotations(source, destination, train_sample, test_sample):
+def create_annotations(source, destination):
+    # Get filenames from filenames.pickle
+    train_sample, test_sample = get_train_test_filenames(root=destination)
+
+    tqdm.write("Train sample %s " % train_sample)
+    tqdm.write("Test sample %s" % test_sample)
+    tqdm.write("Creating annotations...")
     keys = {
-        "images": {"check_key": "file_name", "del_key": "id", "extension": ".jpg"},
-        "annotations": {"check_key": "image_id", "del_key": "category_id", "extension": ""},
-        "categories": {"check_key": "id", "del_key": None, "extension": ""}
+        "images": {"check_key": "file_name", "control_key": "id", "extension": ".jpg"},
+        "annotations": {"check_key": "image_id", "control_key": "category_id", "extension": ""},
+        "categories": {"check_key": "id", "control_key": None, "extension": ""}
     }
 
     keys_without_cat = copy.deepcopy(keys)
+    keys_without_cat["annotations"]["control_key"] = None
     del keys_without_cat["categories"]
 
-    create_json(os.path.join(source, 'annotations/captions_train2014.json'),
-                os.path.join(destination, 'annotations/captions_train2014.json'),
-                train_sample, keys_without_cat)
-    create_json(os.path.join(source, 'annotations/captions_val2014.json'),
-                os.path.join(destination, 'annotations/captions_val2014.json'),
-                test_sample, keys_without_cat)
+    json_files = [{"json_name": "captions_train2014.json", "keys": keys_without_cat, "filenames": train_sample},
+                  {"json_name": "captions_val2014.json", "keys": keys_without_cat, "filenames": test_sample},
+                  {"json_name": "instances_train2014.json", "keys": keys, "filenames": train_sample},
+                  {"json_name": "instances_val2014.json", "keys": keys, "filenames": test_sample},
+                  {"json_name": "person_keypoints_train2014.json", "keys": keys, "filenames": train_sample},
+                  {"json_name": "person_keypoints_val2014.json", "keys": keys, "filenames": test_sample}]
 
-    create_json(os.path.join(source, 'annotations/instances_train2014.json'),
-                os.path.join(destination, 'annotations/instances_train2014.json'),
-                train_sample, keys)
-    create_json(os.path.join(source, 'annotations/instances_val2014.json'),
-                os.path.join(destination, 'annotations/instances_val2014.json'),
-                test_sample, keys)
-    create_json(os.path.join(source, 'annotations/person_keypoints_train2014.json'),
-                os.path.join(destination, 'annotations/person_keypoints_train2014.json'),
-                train_sample, keys)
+    for json_file in json_files:
+        create_json(old_root=source,
+                    new_root=destination,
+                    json_name=json_file["json_name"],
+                    filenames=json_file["filenames"],
+                    keys=json_file["keys"])
 
-    create_json(os.path.join(source, 'annotations/person_keypoints_val2014.json'),
-                os.path.join(destination, 'annotations/person_keypoints_val2014.json'),
-                test_sample, keys)
+def gen_ex_fnames_n_caps(caption_file_path, example_filenames_dest, example_captions_dest, n_files=9):
 
-
-def gen_ex_fnames_n_caps(caption_file_path, example_filenames_dest, example_captions_dest, filenames, n_files=9):
+    tqdm.write("Generating example filenames and captions...")
     captions_dict = load_json(caption_file_path)
     images = captions_dict["images"]
     annotations = captions_dict["annotations"]
 
+    # Remove files if they already exist
+    if os.path.isfile(example_filenames_dest):
+        os.remove(example_captions_dest)
+    if os.path.isfile(example_captions_dest):
+        os.remove(example_captions_dest)
+
     example_filenames_file = open(example_filenames_dest, 'w', encoding='utf-8')
-    example_captions_file = open(example_captions_dest, 'w', encoding='uft-8')
+    example_captions_file = open(example_captions_dest, 'w', encoding='utf-8')
 
     example_filenames_file.write("interpolate_captions\n")
     example_filenames_file.write("example_captions\n")
 
-    for i in trange(n_files):
+    i = 0
+    while i < n_files:
         idx = randint(0, len(images)-1)
         img_id = images[idx]["id"]
+        filename = images[idx]["file_name"].replace('.jpg', '')
         # Find corresponding caption
-        ann_dict = next(ann for ann in annotations if ann["image_id"] == img_id)
-        caption = ann_dict["caption"]
+        try:
+            ann_dict = next(ann for ann in annotations if ann["image_id"] == img_id)
+            caption = ann_dict["caption"]
+            example_filenames_file.write("text/%s\n" % filename)
+            example_captions_file.write("%s\n" % caption)
+            i += 1
+            tqdm.write("Found corresponding caption for image with id %s" % str(img_id))
+        except StopIteration:
+            tqdm.write("Could not find corresponding caption for image with id %s" % str(img_id))
 
-        filename = filenames.pop(img_id)
-        example_filenames_file.write("text/%s\n" % filename)
-        example_captions_file.write("%s\n" % caption)
-
-    print("Done creating %s and %s" % (example_filenames_dest, example_captions_dest))
+    tqdm.write("Done creating %s and %s" % (example_filenames_dest, example_captions_dest))
 
 
-def copy_text_files(source, destination, train_test_filenames):
+def copy_text_files(source, destination):
+    tqdm.write("Copying text files...")
+    train_filenames, test_filenames = get_train_test_filenames(root=destination)
+    train_test_filenames = train_filenames + test_filenames
     copy_files(source=os.path.join(source, 'text'),
                destination=os.path.join(destination, 'text'),
                extension='.txt',
                filenames=train_test_filenames)
 
 
-def copy_images(source, destination, folders, train_sample, test_sample):
+def copy_images(source, destination, folders):
+    tqdm.write("Copying images...")
+    train_filenames, test_filenames = get_train_test_filenames(root=destination)
     filenames=[]
     for folder_name in tqdm(folders):
         if "train" in folder_name:
-            filenames = train_sample
+            filenames = train_filenames
         elif "val" in folder_name:
-            filenames = test_sample
+            filenames = test_filenames
 
         copy_files(source=os.path.join(source, folder_name),
                    destination=os.path.join(destination, folder_name),
@@ -225,52 +281,59 @@ def copy_images(source, destination, folders, train_sample, test_sample):
 def main(big_root, small_root):
     # GET SAMPLE OF FILENAMES FROM big_root AND SAVE THEM TO small_root
     percentage = 0.05
-    train_filenames, test_filenames = get_sample_filenames(big_root, percentage)
+    train_filenames, test_filenames = generate_sample_filenames(big_root, percentage)
     save_samples(small_root, train_filenames, test_filenames)
 
     # GENERATE ALL FILES IN ANNOTATIONS AND FILTER OUT ALL ENTRIES THAT DO NOT CORRESPOND TO THE SAMPLE FILENAMES
-    create_annotations(source=big_root,
-                       destination=small_root,
-                       train_sample=train_filenames,
-                       test_sample=test_filenames)
+    create_annotations(source=big_root, destination=small_root)
 
     # Generate example_captions.txt, example_filenames.txt
+    # TODO: Move variable declarations into method?
+
     filenames_dest = os.path.join(small_root, 'example_filenames.txt')
     captions_dest = os.path.join(small_root, 'example_captions.txt')
     cap_path = os.path.join(small_root, 'annotations/captions_val2014.json')
     gen_ex_fnames_n_caps(caption_file_path=cap_path,
                          example_filenames_dest=filenames_dest,
-                         example_captions_dest=captions_dest,
-                         filenames=test_filenames)
+                         example_captions_dest=captions_dest)
 
     # COPY TEXT FILES CORRESPONDING TO THE SAMPLE FILENAMES FROM big_root TO small_root
-    train_test_filenames = train_filenames + test_filenames
-    copy_files(source=os.path.join(big_root, 'text'),
-               destination=os.path.join(small_root, 'text'),
-               extension='.txt',
-               filenames=train_test_filenames)
+    copy_text_files(source=big_root, destination=small_root)
 
     # COPY IMAGES CORRESPONDING TO THE SAMPLE FILENAMES FROM big_root TO small_root
     # COPY IMAGES IN train2014/ and val2014
     folder_names = ["train2014", "val2014", "train2014_resized", "val2014_resized"]
     copy_images(source=big_root,
                 destination=small_root,
-                folders=folder_names,
-                train_sample=train_filenames,
-                test_sample=test_filenames)
+                folders=folder_names)
 
+def test_create_annotations():
+    filenames = ["COCO_val2014_000000391895"]
+    test_json = {
+        "images": [{"id": 391895, "file_name": "COCO_val2014_000000391895"},
+                   {"id": 522418, "file_name": "COCO_val2014_000000522418"}],
+        "annotations": [{"image_id": 391895, "id": 156, "category_id": 1},
+                        {"image_id": 522418, "id": 509, "category_id": 2}],
+        "categories": [{"id": 1}, {"id": 2}]
+    }
+
+    new_json, control_list = filter_dict(test_json, "images", check_key="file_name", control_list=filenames,
+                                     extension=".jpg", control_key="id")
+    print("new_json: ", new_json)
+    print("control_list: ", control_list)
+    new_json, control_list = filter_dict(new_json, "annotations", check_key="image_id", control_list=control_list,
+                                     control_key="category_id")
+    print("new_json: ", new_json)
+    print("control_list: ", control_list)
+    new_json, control_list = filter_dict(new_json, "categories", check_key="id", control_list=control_list)
+    print("new_json: ", new_json)
+    print("control_list: ", control_list)
 
 if __name__ == '__main__':
     big_root = '../data/big'
     small_root = '../data/small'
 
     main(big_root, small_root)
-
-
-
-
-
-
 
 
 
