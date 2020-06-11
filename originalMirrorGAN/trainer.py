fc0d2eea4e318af3aad971d3363540a9bddab31d
Camilla <cammiida@gmail.com>
2020-06-11 09:16:19 +0200

    :bug:Fix getting scalar tensors with .item()

--- originalMirrorGAN/trainer.py
+++ originalMirrorGAN/trainer.py
@@ -281,7 +281,7 @@ class Trainer(object):
                     errD.backward()
                     optimizersD[i].step()
                     errD_total += errD
-                    D_logs += 'errD%d: %.2f ' % (i, errD.data[0])
+                    D_logs += 'errD%d: %.2f ' % (i, errD.data.item())
 
                 # (4) Update G network: maximize log(D(G(z)))
                 # compute total loss for training G
@@ -293,7 +293,7 @@ class Trainer(object):
                                    words_embs, sent_emb, match_labels, cap_lens, class_ids)
                 kl_loss = KL_loss(mu, logvar)
                 errG_total += kl_loss
-                G_logs += 'kl_loss: %.2f ' % kl_loss.data[0]
+                G_logs += 'kl_loss: %.2f ' % kl_loss.data.item()
                 # backward and update parameters
                 errG_total.backward()
                 optimizerG.step()
@@ -315,7 +315,7 @@ class Trainer(object):
             print('''[%d/%d][%d]
                   Loss_D: %.2f Loss_G: %.2f Time: %.2fs'''
                   % (epoch, self.max_epoch, self.num_batches,
-                     errD_total.data[0], errG_total.data[0],
+                     errD_total.data.item(), errG_total.data.item(),
                      end_t - start_t))
 
             if epoch % cfg.TRAIN.SNAPSHOT_INTERVAL == 0:  # and epoch != 0:
